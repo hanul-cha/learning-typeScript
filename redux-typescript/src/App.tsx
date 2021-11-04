888493b7d6f016633c8dc58a1c85060d92ee5dc8
hanul-cha <ckgks3014@naver.com>
2021-11-04 20:25:38 +0900

    비동기 호출 성공

--- redux-typescript/src/App.tsx
+++ redux-typescript/src/App.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootReducerType } from "./Store"
+import { fetchPokemonData } from "./actions/PokemonActions"
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("")
+  const pokemonReducer = useSelector((state: RootReducerType) => state.PokemonReducer)
+  const dispatch = useDispatch()
+
+
+  const handlePokemonName = (event: React.ChangeEvent<HTMLInputElement>) => setPokemonName(event.target.value)
+  const searchPokemonv = () => {
+    dispatch(fetchPokemonData(pokemonName))
+    console.log(pokemonReducer.pokemon)
+  }
+
+
+
   return (
     <div className="App">
-      asd
+      <input value={pokemonName} onChange={handlePokemonName} />
+      <button onClick={searchPokemonv}>포켓몬찾기</button>
+      {pokemonReducer.success &&
+        <div>
+            <p>{pokemonName}</p>
+            {pokemonReducer.pokemon?.abilities.map(abilities => {
+              return (
+                <div>
+                  <p>{abilities.ability.name}</p>
+                  <p>{abilities.slot}</p>
+                </div>
+              )
+            })}
+            <img src={pokemonReducer.pokemon?.sprites.front_default} alt="" />
+        </div>
+      }
     </div>
   );
 }
