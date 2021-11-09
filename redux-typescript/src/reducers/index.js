9f3cc1d5c07d1586f6f52cd5f60393ae39d0ecbc
hanul-cha <ckgks3014@naver.com>
2021-11-09 16:43:51 +0900

    상위 폴더에서 tsc해서 생긴 문제 해결

--- redux-typescript/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const redux_1 = require("redux"); //여기선 하나만 쓸건데 다른프로젝트에선 여러게 일수 있으니 합쳐서 사용하겠음
-const PokemonReducer_1 = require("./PokemonReducer");
-const rootReducer = (0, redux_1.combineReducers)({
-    PokemonReducer: PokemonReducer_1.default
-});
-exports.default = rootReducer;
